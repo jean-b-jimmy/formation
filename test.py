5d0328598f4d80ddf473bb1a5e490e41fc8dffa0
jean-b-jimmy <jean-baptiste.calvez@jimmy-energy.eu>
2023-09-05 17:18:20 +0200

    Ajout du fichier test.py

---
+++ test.py
@@ -0,0 +1,35 @@
+def afficher_message():
+    print("ok")
+    
+    a = "premier"
+    b = "second"
+    
+    return a,b
+    
+def deuxierme(a,b):
+    
+    print(a)
+    print(b)  
+    
+def choixcalcul():
+    
+    choix = '4'
+    
+    match choix:
+        case '1':
+            result = addition(num1, num2)
+        case '2':
+            result = soustraction(num1, num2)
+        case '3':
+            result = multiplication(num1, num2)
+        case '4':
+            result = "r"
+        case _:
+            print("Choix invalide.")    
+        
+    print(result)
+    
+if __name__=="__main__":
+    a,b = afficher_message()
+    deuxierme(a,b)
+    choixcalcul()
